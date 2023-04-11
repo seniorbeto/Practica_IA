3f665d9db0d03909a38c6e2a2afebaf8cf8743d4
Beto <100471939@alumnos.uc3m.es>
2023-04-11 17:55:49 +0200

    Métodos __str__ y corrección de la lista self.states

--- thermostat.py
+++ thermostat.py
@@ -15,7 +15,12 @@ class Action():
         self.__current_state = current_state
         self.__cost = cost
         self.__probabilities = probabilities 
-    
+
+    def __str__(self):
+        asociated_state = "Estado asociado a la acción: " + self.__current_state + ". "
+        coste = "Con coste: " + str(self.__cost) + ". "
+        return asociated_state + coste
+
     @property 
     def cost(self):
         return self.__cost
@@ -31,20 +36,25 @@ class Action():
 
 
 class State():
-    def __init__(self, id: str, actions: list = None):
+    def __init__(self, id: str, actions: list = []):
         """ 
         State is a class that contains the information of a state
         actions is a list of the Actions that can be done in the state
         """
         self.__state = id
         self.__actions = actions
+
+    def __str__(self):
+        num_actions = "State actions: " + str(len(self.__actions))
+        return "State id: " + self.__state + "  " + num_actions
     
     @property
     def actions(self):
         return self.__actions
 
     def set_action(self, action: Action):
-        self.__actions.append(action)
+        if action not in self.__actions:
+            self.__actions.append(action)
 
     @property
     def id(self):
@@ -59,20 +69,27 @@ class Main():
         self.data_OFF = self.__dataframe_creation("data/TABLA DE TRANSICIONES - OFF.csv")
         
         states_df = self.data_ON.columns.values.tolist() # get states from dataframe
+        # Quitamos los estados generados erróneamente (BUG)
+        states_df.remove("17.1")
+        states_df.remove("17.5.1")
+
         self.states = [] # List of states of type(State)
 
         # create actions list for each state
         i = 0
         for state in states_df:
             probabilities_ON = dict(self.data_ON.iloc[i])
-            action_on = Action(COST_ON, probabilities_ON, state.id)
+            action_on = Action(COST_ON, probabilities_ON, str(state))
             probabilities_OFF = dict(self.data_OFF.iloc[i])
-            action_off = Action(COST_OFF, probabilities_OFF, state.id)
-            my_state = State(str(state))
-            my_state.set_action(action_on)
-            my_state.set_action(action_off)
-            self.states.append(my_state)
+            action_off = Action(COST_OFF, probabilities_OFF, str(state))
+            actions = [action_on, action_off]
+            self.states.append(State(str(state), actions))
             i += 1
+
+        for i in self.states:
+            print(i)
+            for action in i.actions:
+                print (action)
 
     def __dataframe_creation(self, file) -> pd.DataFrame:
         """
@@ -80,9 +97,8 @@ class Main():
         :param file:
         :return:
         """
-        data_frame = pd.read_csv(file,index_col=0)  
+        data_frame = pd.read_csv(file, index_col=0, sep=',')
         data_frame.fillna(0, inplace=True)
-        #print(data_frame)
         return data_frame
 
     @staticmethod
