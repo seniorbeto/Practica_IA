24e8fcab379f8fa7760ef82c02c0df245f511d32
100471939 <100471939@alumnos.uc3m.es>
2023-04-26 09:41:37 +0200

    Generación de gráficas en condiciones

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def simulate(thermostat:Thermostat, iterations: int):
     """
     states_log = []
     state = rnd.choice(thermostat.states)
-    #state = thermostat.states[0]
+    #state = thermostat.states[-1]
     states_log.append(str(state))
     for i in range(iterations-1):
         action = state.prefered_action
@@ -34,13 +34,24 @@ def draw_graph(states_log:list):
               "19", "19,5", "20", "20,5", "21", "21,5", 
               "22", "22,5", "23", "23,5", "24", "24,5", 
               "25"]
+    obj = [22 for i in range(len(states_log))]
+    states_log_ints = []
+    for i in states_log:
+        states_log_ints.append(float(i))
     eje_x = list(range(1, len(states_log) + 1))
-    plt.plot(eje_x, states_log)
     plt.xlim([1, len(states_log)])
-    plt.ylim([1, len(states)])
-    plt.yticks(range(len(states)), states)
+    plt.ylim(16, len(states)+6)
+    plt.yticks([16.0, 16.5, 17.0, 17.5, 18.0, 18.5, 19.0, 19.5,
+                20.0, 20.5, 21.0, 21.5, 22.0, 22.5, 23.0, 23.5,
+                24.0, 24.5, 25.0])
     plt.grid(True)
+    plt.ylabel('Estado')
+    plt.xlabel('Iteración (30 mins/iteración)')
+    plt.title("Simulación del termostato")
+    plt.plot(eje_x, obj, color='r')
+    plt.plot(eje_x, states_log_ints, marker='.')
     plt.show()
+
 
 thermostat = Thermostat("data/TABLA DE TRANSICIONES - ON.csv",
                         "data/TABLA DE TRANSICIONES - OFF.csv",
@@ -48,7 +59,7 @@ thermostat = Thermostat("data/TABLA DE TRANSICIONES - ON.csv",
                         cost_on=1,
                         cost_off=0.3)
 
-a = simulate(thermostat, 100)
+a = simulate(thermostat, 20)
 print(a)
 draw_graph(a)
 
